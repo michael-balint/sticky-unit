1f6bc1676cb0a7fe5c447aee0fd8dd6106720814
Balint, Michael <michael.balint@accenture.com>
2014-07-27 22:31:17 -0700

    Closing modal when standards are selected.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,11 +3,13 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   unit: function() {
     var unit = this.store.find("unit", "main-unit");
+    var controller = this;
 
     unit.then(function(unit) {
       window.changeUnit = function(newUnit) {
         unit.set('standard', newUnit.standard);
         unit.set('detail', newUnit.detail);
+        controller.send('closeModal');
       };
     });
 
